ef46b5dc075a9664a15d58e2a8434dd19e74db2f
Georgi Georgiev <geovgiev@gmail.com>
2026-01-14 10:11:20 +0200

    Added Py file for exercise

---
+++ config.py
@@ -0,0 +1,11 @@
+# This is a test file
+
+first_name = 'Ivo'
+second_name = 'Goshov'
+
+age = 32
+
+email = 'ivo.goshov@dir.bg'
+
+print(f'Hello {first_name} {second_name}. You are {age} years old and your email is {email}')
+
